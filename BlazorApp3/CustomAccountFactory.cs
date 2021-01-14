cbaf076dbe3481140159b3012b57d8fdd760d582
Senso4s <Samo-samo.simoncic@senso4s.com>
2021-01-14 09:52:36 +0100

    Add project files.

---
+++ BlazorApp3/CustomAccountFactory.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication.Internal;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Graph;
+
+namespace BlazorApp3
+{
+    public class CustomAccountFactory
+     : AccountClaimsPrincipalFactory<RemoteUserAccount>
+    {
+        private readonly ILogger<CustomAccountFactory> logger;
+        private readonly IServiceProvider serviceProvider;
+
+        public CustomAccountFactory(IAccessTokenProviderAccessor accessor,
+            IServiceProvider serviceProvider,
+            ILogger<CustomAccountFactory> logger)
+            : base(accessor)
+        {
+            this.serviceProvider = serviceProvider;
+            this.logger = logger;
+        }
+
+        public async override ValueTask<ClaimsPrincipal> CreateUserAsync(
+            RemoteUserAccount account,
+            RemoteAuthenticationUserOptions options)
+        {
+            var initialUser = await base.CreateUserAsync(account, options);
+
+            if (initialUser.Identity.IsAuthenticated)
+            {
+                var userIdentity = (ClaimsIdentity)initialUser.Identity;
+
+                try
+                {
+                    var graphClient = ActivatorUtilities
+                        .CreateInstance<GraphServiceClient>(serviceProvider);
+                    var request = graphClient.Me.Request();
+                    var user = await request.GetAsync();
+
+                    if (user != null)
+                    {
+                        userIdentity.AddClaim(new Claim("mobilephone",
+                            user.MobilePhone));
+                    }
+                }
+                catch (ServiceException exception)
+                {
+                    logger.LogError("Graph API service failure: {Message}",
+                        exception.Message);
+                }
+            }
+
+            return initialUser;
+        }
+    }
+}
---
+++ BlazorApp3/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp3
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            
+            //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddGraphClient("https://graph.microsoft.com/User.Read");
+
+            builder.Services.AddMsalAuthentication(options =>
+             {
+                 builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+                 options.ProviderOptions.DefaultAccessTokenScopes.Add("https://graph.microsoft.com/User.Read");
+                
+             });
+
+            
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
